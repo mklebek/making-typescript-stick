1bd734bc319c7cd52bf46cbdde3322db283f8e32
Michal Klebek <michal.klebek@gmail.com>
2022-09-06 13:54:02 +0200

    Add code for challenge 1

--- challenges/data-layer/src/index.ts
+++ challenges/data-layer/src/index.ts
@@ -13,4 +13,37 @@ export type DataEntityMap = {
   song: Song;
 };
 
-export class DataStore {}
+type DataStoreMethods = {
+  [K in keyof DataEntityMap as `getAll${Capitalize<K>}s`]: () => DataEntityMap[K][];
+} & {
+  [K in keyof DataEntityMap as `get${Capitalize<K>}`]: (
+    id: string
+  ) => DataEntityMap[K];
+} & {
+  [K in keyof DataEntityMap as `clearAll${Capitalize<K>}s`]: () => void;
+};
+
+export class DataStore implements DataStoreMethods {
+  #data: { [K in keyof DataEntityMap]: Record<string, DataEntityMap[K]> } = {
+    movie: {},
+    song: {},
+  };
+  getAllSongs(): Song[] {
+    return [];
+  }
+  getAllMovies(): Movie[] {
+    return [];
+  }
+  getSong(id: string): Song {
+    return { singer: "", id };
+  }
+  getMovie(id: string): Movie {
+    return { director: "", id };
+  }
+  clearAllSongs(): void {}
+  clearAllMovies(): void {}
+}
+
+const ts: DataStoreMethods = {} as any;
+
+const ds = new DataStore();
