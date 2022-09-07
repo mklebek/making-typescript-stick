bd942b5f8f9e912073f44c0fd3b6a622a5504467
Michal Klebek <michal.klebek@gmail.com>
2022-09-07 10:43:20 +0200

    Solution for jQuery challenge

--- challenges/jquery-clone/src/index.ts
+++ challenges/jquery-clone/src/index.ts
@@ -1,12 +1,51 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
 function $(selector: string): any {
-  return {} as any;
+  return {
+    html(arg: string): string {
+      const element = document.body.querySelector(selector);
+      if (element) {
+        element.innerHTML = arg;
+      }
+      return arg;
+    },
+    hide() {
+      const element = document.body.querySelector(
+        selector
+      ) as HTMLButtonElement;
+      if (element && element.style) {
+        element.style.visibility = 'hidden';
+      }
+    },
+    show() {
+      const element = document.body.querySelector(
+        selector
+      ) as HTMLButtonElement;
+      if (element && element.style) {
+        element.style.visibility = 'visible';
+      }
+    },
+    on(method: string, callback: EventListenerOrEventListenerObject) {
+      const element = document.body.querySelector(
+        selector
+      ) as HTMLButtonElement;
+      if (element) {
+        element.addEventListener(method, callback);
+      }
+    },
+  } as any;
 }
 
 namespace $ {
   export function ajax(...args: any[]): any {
-    return {} as any;
+    const [options] = args;
+    const { url, success } = options;
+    return fetch(url)
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        success(responseJSON);
+        return responseJSON;
+      });
   }
 }
 
--- challenges/jquery-clone/tests/core-api.test.ts
+++ challenges/jquery-clone/tests/core-api.test.ts
@@ -1,113 +1,113 @@
 /**
  * @jest-environment jsdom
  */
-import $ from "../src";
+import $ from '../src';
 
 test("module's default export is a value", () => {
   expect($).toBeTruthy();
 });
 
 test("module's default export is a one-arg function", () => {
-  expect(typeof $).toBe("function");
+  expect(typeof $).toBe('function');
   expect($.length).toBe(1);
 });
 
 test("module's default export has an `.ajax` property that's a function", () => {
-  expect(typeof $.ajax).toBe("function");
+  expect(typeof $.ajax).toBe('function');
 });
 
 test("module's default export has an `.ajax` property that's a function", () => {
-  expect(typeof $.ajax).toBe("function");
+  expect(typeof $.ajax).toBe('function');
 });
 
 test('`$("button.continue")` returns something truthy', () => {
-  const element = document.createElement("div");
-  element.id = "test-element";
+  const element = document.createElement('div');
+  element.id = 'test-element';
   element.innerHTML = `<button class='continue'>Continue</button>`;
   document.body.appendChild(element);
 
-  const result = $("button.continue");
+  const result = $('button.continue');
   expect(result).toBeTruthy();
 
   document.body.removeChild(element);
 });
 
 test('`$("button.continue").html("Next Step...")` returns something truthy', () => {
-  const element = document.createElement("div");
-  element.id = "test-element";
+  const element = document.createElement('div');
+  element.id = 'test-element';
   element.innerHTML = `<button class='continue'>Continue</button>`;
   document.body.appendChild(element);
 
-  const result = $("button.continue");
+  const result = $('button.continue');
   expect(result).toBeTruthy();
-  result.html("Next Step...");
-  expect(document.querySelector("button.continue")!.innerHTML).toBe(
-    "Next Step..."
+  result.html('Next Step...');
+  expect(document.querySelector('button.continue')!.innerHTML).toBe(
+    'Next Step...'
   );
 
   document.body.removeChild(element);
 });
 test('`$("button.continue").hide()` makes button "visibility:hidden"', () => {
-  const element = document.createElement("div");
-  element.id = "test-element";
+  const element = document.createElement('div');
+  element.id = 'test-element';
   element.innerHTML = `<button class='continue'>Continue</button>`;
   document.body.appendChild(element);
 
-  const result = $("button.continue");
+  const result = $('button.continue');
   expect(result).toBeTruthy();
   result.hide();
-  const button = document.querySelector("button.continue") as HTMLButtonElement;
-  expect(button.style.visibility).toBe("hidden");
+  const button = document.querySelector('button.continue') as HTMLButtonElement;
+  expect(button.style.visibility).toBe('hidden');
 
   document.body.removeChild(element);
 });
 test('`$("button.continue").hide()` makes button "visibility:hidden"', () => {
-  const element = document.createElement("div");
-  element.id = "test-element";
+  const element = document.createElement('div');
+  element.id = 'test-element';
   element.innerHTML = `<button class='continue'>Continue</button>`;
   document.body.appendChild(element);
 
-  const result = $("button.continue");
+  const result = $('button.continue');
   expect(result).toBeTruthy();
   result.hide();
-  const button = document.querySelector("button.continue") as HTMLButtonElement;
-  expect(button.style.visibility).toBe("hidden");
+  const button = document.querySelector('button.continue') as HTMLButtonElement;
+  expect(button.style.visibility).toBe('hidden');
   result.show();
-  expect(button.style.visibility).toBe("visible");
+  expect(button.style.visibility).toBe('visible');
 
   document.body.removeChild(element);
 });
 test('`$("button.continue").on("click", () => {})` registers a working event listener', () => {
-  const element = document.createElement("div");
-  element.id = "test-element";
+  const element = document.createElement('div');
+  element.id = 'test-element';
   element.innerHTML = `<button class='continue'>Continue</button>`;
   document.body.appendChild(element);
 
-  const result = $("button.continue");
+  const result = $('button.continue');
   expect(result).toBeTruthy();
   let clickCount = 0;
-  result.on("click", function (evt) {
+  result.on('click', function (evt: any) {
     clickCount++;
   });
-  const button = document.querySelector("button.continue") as HTMLButtonElement;
+  const button = document.querySelector('button.continue') as HTMLButtonElement;
   expect(clickCount).toBe(0);
   button.click();
   expect(clickCount).toBe(1);
 
   document.body.removeChild(element);
 });
-test("`$.ajax test", async () => {
-  const element = document.createElement("div");
-  element.id = "test-element";
+test('`$.ajax test', async () => {
+  const element = document.createElement('div');
+  element.id = 'test-element';
   element.innerHTML = `<div id='post-info'></div>`;
   document.body.appendChild(element);
 
   const result = await $.ajax({
-    url: "https://jsonplaceholder.typicode.com/posts/33",
-    success: (result) => {
-      if (result && typeof result === "object" && !Array.isArray(result)) {
-        $("#post-info").html(
-          "<strong>" + result.title + "</strong>" + result.body
+    url: 'https://jsonplaceholder.typicode.com/posts/33',
+    success: (result: any) => {
+      if (result && typeof result === 'object' && !Array.isArray(result)) {
+        $('#post-info').html(
+          '<strong>' + result.title + '</strong>' + result.body
         );
       }
     },
